0110a23ffb0fd52e17113eca50c5b39ef6f7d29e
729950490@qq.com <tengjiqi123>
2017-04-04 23:27:44 +0800

    完成 SwiftyPhotoClipper

---
+++ SwiftyPhotoClipper/SwiftyPhotoClipper/SwiftyPhotoClipper.swift
@@ -0,0 +1,213 @@
+//
+//  SwiftyPhotoClipper.swift
+//  SwiftyPhotoClipper
+//
+//  Created by lip on 17/4/4.
+//  Copyright © 2017年 lip. All rights reserved.
+//
+
+import UIKit
+
+//
+//  UIClipController.swift
+//  Temple
+//
+//  Created by lip on 17/4/4.
+//  Copyright © 2017年 lip. All rights reserved.
+//
+
+import UIKit
+
+protocol SwiftyPhotoClipperDelegate {
+    
+    func didFinishClippingPhoto(image:UIImage)
+}
+
+class SwiftyPhotoClipper: UIViewController {
+    
+    // 代理
+    var delegate:SwiftyPhotoClipperDelegate?
+    
+    var imgView:UIImageView?
+    
+    var img:UIImage?
+    let scrollview = UIScrollView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+    
+    var maxScale:CGFloat = 3.0
+    var minScale:CGFloat = 1.0
+    
+    // 屏幕
+    let HEIGHT = UIScreen.main.bounds.height
+    let WIDTH = UIScreen.main.bounds.width
+    
+    // 截图大小
+    var selectWidth:CGFloat = UIScreen.main.bounds.width
+    var selectHeight:CGFloat = 200.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupUI()
+        drawTheRect()
+    }
+    
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /// 设置图片
+    func setImageView(image:UIImage){
+        imgView = UIImageView(image: image)
+    }
+    
+    
+    
+}
+
+// MARK: - UI
+extension SwiftyPhotoClipper{
+    
+    fileprivate func setupUI(){
+        
+        scrollview.backgroundColor = UIColor.black
+        
+        imgView = UIImageView(image: img)
+        
+        guard let imgView = imgView else {
+            return
+        }
+        view.backgroundColor = UIColor.white
+        imgView.contentMode = .scaleToFill
+        scrollview.delegate = self
+        imgView.center = scrollview.center
+        
+        if imgView.bounds.width > WIDTH {
+            imgView.frame.size = CGSize(width: WIDTH, height: imgView.bounds.height / imgView.bounds.width * WIDTH)
+            imgView.center = scrollview.center
+        }
+        if imgView.bounds.height > HEIGHT{
+            imgView.frame.size = CGSize(width: HEIGHT, height: imgView.bounds.width / imgView.bounds.height * HEIGHT)
+            imgView.center = scrollview.center
+        }
+        
+        view.addSubview(scrollview)
+        scrollview.addSubview(imgView)
+        
+        // 设置缩放属性
+        scrollview.maximumZoomScale = maxScale
+        scrollview.minimumZoomScale = minScale
+        
+        // 设置按钮
+        
+        let cancelBtn = UIButton(frame: CGRect(x: 10, y: HEIGHT - 50, width: 100, height: 40))
+        cancelBtn.setTitle("取消", for: .normal)
+        cancelBtn.addTarget(nil, action: #selector(cancelBtnIsClicked), for: .touchUpInside)
+        view.addSubview(cancelBtn)
+        
+        let okBtn = UIButton(frame: CGRect(x: WIDTH - 110, y: HEIGHT - 50, width: 100, height: 40))
+        okBtn.contentMode = .right
+        okBtn.setTitle("选取", for: .normal)
+        okBtn.addTarget(nil, action: #selector(okBtnIsClicked), for: .touchUpInside)
+        view.addSubview(okBtn)
+        
+        
+        
+    }
+    
+    fileprivate func clipImage()->UIImage?{
+        
+        let rect  = UIScreen.main.bounds
+        
+        // 记录屏幕缩放比
+        let scal = UIScreen.main.scale
+        
+        // 上下文
+        UIGraphicsBeginImageContextWithOptions(rect.size, true, 0)
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        UIApplication.shared.keyWindow?.layer.render(in: context!)
+        
+        // 截全屏
+        guard let img = UIGraphicsGetImageFromCurrentImageContext()?.cgImage,
+            let result = img.cropping(to: CGRect(x: 0, y: (HEIGHT - selectHeight)/2 * scal, width: self.WIDTH * scal, height: selectHeight * scal))   else{
+                return nil
+        }
+        // 关闭上下文
+        UIGraphicsEndImageContext()
+        
+        return UIImage(cgImage: result, scale: scal, orientation: .up)
+        
+    }
+    
+    
+    /// 绘制选择框
+    fileprivate func drawTheRect(){
+        
+        
+        // 获取上下文 size表示图片大小 false表示透明 0表示自动适配屏幕大小
+        UIGraphicsBeginImageContextWithOptions(UIScreen.main.bounds.size, false, 0)
+        
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor)
+        context?.fill(UIScreen.main.bounds)
+        context?.addRect(CGRect(x: 0, y: (HEIGHT - selectHeight)/2, width: WIDTH , height: selectHeight))
+        context?.setBlendMode(.clear)
+        context?.fillPath()
+        
+        
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let selectarea = UIImageView(image: img)
+        selectarea.frame.origin = CGPoint(x: 0, y: 0)
+        view.addSubview(selectarea)
+        
+        
+    }
+    
+}
+
+// MARK: - 代理方法
+extension SwiftyPhotoClipper:UIScrollViewDelegate{
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        
+        //当捏或移动时，需要对center重新定义以达到正确显示位置
+        var centerX = scrollView.center.x
+        var centerY = scrollView.center.y
+        centerX = scrollView.contentSize.width > scrollView.frame.size.width ? scrollView.contentSize.width / 2 : centerX
+        centerY = scrollView.contentSize.height > scrollView.frame.size.height ?scrollView.contentSize.height / 2 : centerY
+        self.imgView?.center = CGPoint(x: centerX, y: centerY)
+        
+        
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imgView
+    }
+    
+}
+
+// MARK: - 监听
+extension SwiftyPhotoClipper{
+    
+    @objc fileprivate func cancelBtnIsClicked(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc fileprivate func okBtnIsClicked(){
+        let result = clipImage() ?? UIImage()
+        delegate?.didFinishClippingPhoto(image: result)
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
---
+++ SwiftyPhotoClipper/SwiftyPhotoClipper/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  SwiftyPhotoClipper
+//
+//  Created by lip on 17/4/4.
+//  Copyright © 2017年 lip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.darkGray
+        
+        let photoBtn = UIButton(frame: CGRect(x: 10, y: 100, width: 100, height: 20))
+        photoBtn.setTitle("相册中选择", for: .normal)
+        photoBtn.addTarget(nil, action: #selector(photoBtnIsClicked), for: .touchUpInside)
+        
+        let cameraBtn = UIButton(frame: CGRect(x: 10, y: 200, width: 100, height: 20))
+        cameraBtn.setTitle("拍照", for: .normal)
+        cameraBtn.addTarget(nil, action: #selector(camerBtnIsClicked), for: .touchUpInside)
+        
+        view.addSubview(photoBtn)
+        view.addSubview(cameraBtn)
+     
+    }
+    
+    @objc private func photoBtnIsClicked(){
+        //判断设置是否支持图片库
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            //初始化图片控制器
+            let picker = UIImagePickerController()
+            //设置代理
+            picker.delegate = self
+            //指定图片控制器类型
+            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+
+            //弹出控制器，显示界面
+            self.present(picker, animated: true, completion: nil)
+        }
+
+    }
+    
+    @objc private func camerBtnIsClicked(){
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            //初始化图片控制器
+            let picker = UIImagePickerController()
+            //设置代理
+            picker.delegate = self
+            //指定图片控制器类型
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            //弹出控制器，显示界面
+            self.present(picker, animated: true, completion: nil)
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate,SwiftyPhotoClipperDelegate{
+    //选择图片成功后代理
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+        
+            picker.dismiss(animated: false, completion: { 
+                let clipper = SwiftyPhotoClipper()
+                clipper.delegate = self
+                clipper.img = image
+                self.present(clipper, animated: true, completion: nil)
+            })
+            
+            
+        } else{
+            print("Something went wrong")
+        }
+        
+        
+    }
+    
+    func didFinishClippingPhoto(image: UIImage) {
+        
+        let imgv = UIImageView(frame: CGRect(x: 0, y: 300, width: UIScreen.main.bounds.width, height: 200))
+            imgv.image = image
+        
+        view.addSubview(imgv)
+    }
+
+}
+
